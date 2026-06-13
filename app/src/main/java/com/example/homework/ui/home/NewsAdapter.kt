e909bee132713627528fdb081e8746f976132bfb
10leoM <2549678982@qq.com>
2026-06-14 00:02:18 +0800

    修复编译问题

--- app/src/main/java/com/example/homework/ui/home/NewsAdapter.kt
+++ app/src/main/java/com/example/homework/ui/home/NewsAdapter.kt
@@ -6,6 +6,7 @@ import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.homework.R
 import com.example.homework.databinding.ItemNewsArticleBinding
 import com.example.homework.model.NewsArticle
 import com.example.homework.util.ImageLoadHelper
